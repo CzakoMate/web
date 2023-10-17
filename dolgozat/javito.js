82e952f1a08ff9c2869e3039cb1cd86cad867391
CzakoMate <78041674+CzakoMate@users.noreply.github.com>
2023-10-17 09:34:02 +0200

    Add files via upload

---
+++ dolgozat/javito.js
@@ -0,0 +1,8 @@
+function Szinez()
+{
+document.getElementById("container").style="Background-color:#12406A";
+document.getElementById("szin1").style="Background-color:#EB8F90";
+document.getElementById("szin2").style="Background-color:#FFB471";
+document.getElementById("szin3").style="Background-color:#ADBED2";
+document.getElementById("body").style="Background-color:#CCFFFF";
+}
