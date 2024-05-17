6c01bc9c00099ba808449ddc2905152597ec75bf
CzakoMate <78041674+CzakoMate@users.noreply.github.com>
2024-05-17 11:19:05 +0200

    Update kiiratas.js

--- kurzusok/kiiratas.js
+++ kurzusok/kiiratas.js
@@ -1,18 +1,46 @@
 const url = "https://vvri.pythonanywhere.com/api/courses"
-
-fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data)
-                data.forEach(element => {
-                    console.log(element)
-            })
+  async function fetchData(){
+    try{
+        const response=await fetch(url,{
+            method:"GET",
         })
-        .catch(error => console.log("Hiba történt: " + error))
-    
+        const data=await response.json()
+        if(data){
+            data.forEach(element=>{
+                console.log(element)
+            })
+        }
+    }catch(error){
+        console.log("Hiba történt: "+ error)
+    }
+  }
+  fetchData();
 let matrix=[];
 let valt=10;
 let kezd=0;
+
+async function fetchData2(){
+    try{
+        const response=await fetch(url,{
+            method:"GET",
+        })
+        const data=await response.json()
+        if(data){
+            data.forEach(todo => {
+                matrix.push(tomb=[])
+                    tomb[0]=todo.id
+                    tomb[1]=todo.name
+                    tomb[2]=todo.students
+            });
+            kiir();
+        }
+    }catch(error){
+        console.log("Hiba történt: "+ error)
+    }
+  }
+  fetchData2();
+
+
 function kiir(){
     let ki=""
     if(valt>matrix.length)
@@ -24,8 +52,10 @@ function kiir(){
                     ki+= "</div><br>";
                     kezd++;
                 }
+                
             document.getElementById("users").innerHTML = ki;
             document.getElementById("elore").style.display='none';
+            document.getElementById("hatra").style.display='inline';
         }
     else if(valt<=10){
         for(let i=kezd;i<valt;i++)
@@ -54,56 +84,50 @@ function viszir(){
     let ki=""
     if(valt>matrix.length)
         {
-            kezd=kezd-10;
             for(let i=(matrix.length-(matrix.length%10));i<matrix.length;i++)
                 {
-                    ki+="<div id='kurzus'  class='"+matrix[i][0]+"'>"+matrix[i][0]+"<br>"+matrix[i][1]
-                    for(let j=0;j<matrix[i][2].length;j++){ki+=matrix[i][2][j][1]+" "}
-                    ki+=+ "</div><br>";
-                    kezd++;
+                    ki+="<div id='kurzus'  class='"+matrix[i][0]+"'>"+matrix[i][0]+"<br>"+matrix[i][1]+"<br>"
+                    for(let j=0;j<matrix[i][2].length;j++){ki+=matrix[i][2][j].name+" "}
+                    ki+= "</div><br>";
                 }
-            document.getElementById("users").innerHTML = ki2;
+            document.getElementById("users").innerHTML = ki;
             document.getElementById("elore").style.display='none';
         }
     else if(valt<=10){
         kezd=0;
         for(let i=kezd;i<valt;i++)
             {
-                ki+="<div id='kurzus'  class='"+matrix[i][0]+"'>"+matrix[i][0]+"<br>"+matrix[i][1]
-                    for(let j=0;j<matrix[i][2].length;j++){ki+=matrix[i][2][j][1]+" "}
-                    ki+=+ "</div><br>";
+                ki+="<div id='kurzus'  class='"+matrix[i][0]+"'>"+matrix[i][0]+"<br>"+matrix[i][1]+"<br>"
+                    for(let j=0;j<matrix[i][2].length;j++){ki+=matrix[i][2][j].name+" "}
+                    ki+= "</div><br>";
                 kezd++;
             }
-        document.getElementById("users").innerHTML = ki2;
+        document.getElementById("users").innerHTML = ki;
         document.getElementById("hatra").style.display='none';
+        document.getElementById("elore").style.display='inline';
     }
     else{
-        kezd=kezd-10;
+        if(kezd>=10){
+            kezd=kezd-10;
+        }
+        else{
+            kezd=0;
+        }
             for(let i=kezd;i<valt;i++)
                 {
-                    ki+="<div id='kurzus'  class='"+matrix[i][0]+"'>"+matrix[i][0]+"<br>"+matrix[i][1]
-                    for(let j=0;j<matrix[i][2].length;j++){ki+=matrix[i][2][j][1]+" "}
-                    ki+=+ "</div><br>";
+                    ki+="<div id='kurzus'  class='"+matrix[i][0]+"'>"+matrix[i][0]+"<br>"+matrix[i][1]+"<br>"
+                    for(let j=0;j<matrix[i][2].length;j++){ki+=matrix[i][2][j].name+" "}
+                    ki+= "</div><br>";
                     kezd++;
                 }
-            document.getElementById("users").innerHTML = ki2;
+            document.getElementById("users").innerHTML = ki;
             document.getElementById("hatra").style.display='inline';
             document.getElementById("elore").style.display='inline';
             
     } 
 }
 
-fetch("https://vvri.pythonanywhere.com/api/courses")
-.then(response => response.json())
-.then(json => {
-    json.forEach(todo => {
-        matrix.push(tomb=[])
-            tomb[0]=todo.id
-            tomb[1]=todo.name
-            tomb[2]=todo.students
-    });
-    kiir();
-});
+
 function lapoz(){
     document.getElementById("users").remove();
     const dbz=document.createElement("div");
@@ -121,18 +145,22 @@ function lapozvissza(){
     viszir();
 }
 
-function HozzaAd()
-{
-    let kurzusnev=document.getElementById("Hozzavalo").value.toString();
-    fetch("https://vvri.pythonanywhere.com/api/courses", {
-        method: "POST",
-        body: JSON.stringify({
-          "name": kurzusnev
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8"
+async function HozzaAd(){
+    try{
+        const response=await fetch(url,{
+            method:"POST",
+            body: JSON.stringify({
+                "name": document.getElementById("Hozzavalo").value.toString()
+              }),
+              headers: {
+                "Content-type": "application/json; charset=UTF-8"
+              }
+        })
+        const data=await response.json()
+        if(data){
+            document.getElementById("Hozzavalo").value='';
         }
-      })
-        .then(response => response.json())
-    kurzusnev.value="";
+    }catch(error){
+        console.log("Hiba történt: "+ error)
     }
+  }
